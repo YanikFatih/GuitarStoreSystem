4b13afd58bf08f55fd9a46426309c5f047041fec
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2022-04-11 13:46:34 +0300

    init

---
+++ GuitarStoreSystem/src/GuitarStoreSystem/Type.java
@@ -0,0 +1,15 @@
+package GuitarStoreSystem;
+
+public enum Type {
+	
+	ACOUSTIC, ELECTRIC;
+
+	public String toString() {
+		switch (this) {
+			case ACOUSTIC:	return "acoustic";
+			case ELECTRIC:	return "electric";
+			default:		return "unspecified";
+		}
+	}
+
+}
